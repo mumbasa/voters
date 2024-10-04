f5d7d08e2f854f71bb3e553396bba6fc6abdfc34
bryan <bry_lar@yahoo.com>
2024-10-05 00:43:33 +0100

    first commit

--- src/main/java/com/ghana/voter/controller/VoterController.java
+++ src/main/java/com/ghana/voter/controller/VoterController.java
@@ -1,6 +1,7 @@
 package com.ghana.voter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class VoterController {
 
     @Autowired
     VoterService service;
-
+	@CrossOrigin(origins = "*")
     @GetMapping("/voter/information")
     public Voters getVoterDetail(@RequestParam String number){
     
